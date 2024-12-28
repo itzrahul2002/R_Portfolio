a8b0c8292dd5a9aab042348be2ccdfc86d34ec6e
itzrahul2002 <119419147+itzrahul2002@users.noreply.github.com>
2024-12-28 20:47:41 +0530

    Add files via upload

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes } from 'react-router-dom'
+import './App.css'
+import Footer from './Components/Footer/footer'
+import Home from './Components/Home/home'
+
+function App() {
+  return (
+      <>
+        <Home/>
+        <Footer/>
+      </>
+  )
+}
+
+export default App
